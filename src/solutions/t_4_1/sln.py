81723d664ac0cddcaf39a1a411bdf9e14ed60477
Kirill Nevedrov <kirill.nevedrov@gmail.com>
2023-09-29 22:05:36 +0200

    Add solutions

--- src/solutions/t_4_1/sln.py
+++ src/solutions/t_4_1/sln.py
@@ -2,18 +2,24 @@ from src.graph import Graph, GraphNode
 from src.queue import Queue
 
 def has_route(src_node: GraphNode, trg_node: GraphNode) -> bool:
+    if src_node is None or trg_node is None:
+        return False
+
+    if src_node is trg_node:
+        return True
+
     queue = Queue[GraphNode]()
     src_node.visited = True
     queue.add(src_node)
 
     while not queue.is_empty():
         n = queue.remove()
-
-        if n is trg_node:
-            return True
         
         for c in n.childs:
             if not c.visited:
+                if c is trg_node:
+                    return True
+                
                 c.visited = True
                 queue.add(c)
 
